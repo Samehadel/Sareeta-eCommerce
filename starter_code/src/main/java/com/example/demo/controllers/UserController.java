0796158bd638bcea2e81c76fc522ec461620349f
samehadel <eng.sameh95@outlook.com>
2021-05-30 15:06:23 +0200

    Logback added to resources

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +17,9 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -27,6 +31,8 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		logger.info("Find User By ID Request");
+
 		Optional<User> optionalUser = userRepository.findById(id);
 
 		if (!optionalUser.isPresent())
@@ -37,14 +43,19 @@ public class UserController {
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		logger.info("Find User By Username Request");
 		User user = userRepository.findByUsername(username);
+
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		logger.info("Create User Request");
+
 		User user = new User();
 		Cart cart = new Cart();
+
 		cartRepository.save(cart);
 
 		// Check if username already exists
@@ -57,11 +68,13 @@ public class UserController {
 		    !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword()) ){
 			return ResponseEntity.badRequest().build();
 		}
+
 		user.setUsername(createUserRequest.getUsername());
 		user.setPassword(encoder.encode(createUserRequest.getPassword()));
 		user.setCart(cart);
 
 		userRepository.save(user);
+
 		return ResponseEntity.ok(user);
 	}
 	
