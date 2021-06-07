68edc78d37087681cae2d9b6d38d187dd725b18e
samehadel <eng.sameh95@outlook.com>
2021-06-07 10:47:39 +0200

    Security Issues Fixed

--- starter_code/src/main/java/com/example/demo/security/CustomeAuthenticationEntryPoint.java
+++ starter_code/src/main/java/com/example/demo/security/CustomeAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -11,17 +13,21 @@ import java.io.IOException;
 public class CustomeAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final String headerValue;
+    private static final Logger logger = LoggerFactory.getLogger("splunkLogger");
 
     public CustomeAuthenticationEntryPoint(String headerValue) {
         this.headerValue = headerValue;
     }
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+
         response.setHeader("Authenticate", this.headerValue);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
                 authException.getMessage());
-    }
 
+        logger.info("Failure: Sign In Bad Credentials");
+    }
 }
